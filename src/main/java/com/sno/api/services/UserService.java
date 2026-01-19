21a0910b1f0e8d3d7dee13c6435507c0c61b6096
Mathias Sletten <mathiasasletten@gmail.com>
2026-01-19 16:16:37 -0600

    init

---
+++ src/main/java/com/sno/api/services/UserService.java
@@ -0,0 +1,8 @@
+package com.sno.api.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+}
---
+++ src/test/java/com/sno/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sno.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
